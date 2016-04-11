a6d41abad48bdcdaf69dd141bfcb6d5db10b05b3
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2016-04-11 17:48:10 +0100

    initial

---
+++ modules/ZI/ZIPlayers.js
@@ -0,0 +1,127 @@
+var utils = require('utils');
+
+//Tally of players and zombies
+var numberOfHumans = 0;
+var numberOfZombies = 0;
+
+var zombieString = "Z";
+var humanString = "H";
+
+//Dictionary of players and their status
+var players = {};
+
+//All players currently on the server
+function allPlayersOnServer() {
+	return utils.players();
+}
+
+//Called after a refresh
+function addAllOnlinePlayersAsHuman() {
+	//Clear list
+	players = {};
+
+	//Reset number of humans and zombies
+	numberOfHumans = 0;
+	numberOfZombies = 0;
+
+	//Get list of all online players
+	var plist = allPlayersOnServer();
+	
+	//Make everyone human
+	plist.forEach( function f(e) {
+		makeHuman(e);
+	});
+}
+
+//Make a player a human
+function makeHuman( player ) {
+	print("Making " + player.name + " human");
+	echo(player, "You are currently human");
+	players[player.name] = humanString;
+	//Update count
+	numberOfHumans++;
+};
+
+//Convert a human to a zombie
+function makeZombie( player ) {
+	//Validation check
+	if ( players[player.name] != humanString ) {
+		return;
+	}
+
+	//Update count
+	numberOfZombies++;
+	numberOfHumans--;
+	
+	//Change status
+	print("Making " + player.name + " zombie");
+	echo(player, "You are now a ZOMBIE");
+	players[player.name] = zombieString;
+};
+
+//Query zombie status
+function amIZombie( player ) {
+	if (players[player.name] == zombieString) {
+		return true;
+	} else {
+		return false;
+	}
+};
+//Query human status
+function amIHuman( player ) {
+	if (players[player.name] == humanString) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+//Player exists
+function playerExists( player ) {
+	if (players[player.name]) {
+		return true;
+	} else {
+		return false;
+	} 
+};
+
+//Remove the player attribute
+function deletePlayer( player ) {
+
+	//Update the counts
+	if (players[player.name] == zombieString) {
+		numberOfZombies--;
+		print("Number of zombies is now " + numberOfZombies);
+	} else {
+		numberOfHumans--;
+		print("Number of humans is now " + numberOfHumans);			
+	}
+
+	//Console message
+	print("player " + player.name + " is being removed from the game");
+
+	//Remove from players object
+	delete(players[player.name]);
+};
+
+function numberOfPlayers() {
+	return Object.keys(players).length;
+};
+
+function playerStatus() {
+	var NP = numberOfPlayers();
+	print('Number of players: ' + NP);
+	for (pl in players) {
+		print(pl + " is " + players[pl]);
+	}
+};
+
+exports.addAllOnlinePlayersAsHuman = addAllOnlinePlayersAsHuman;
+exports.makeHuman = makeHuman;
+exports.makeZombie = makeZombie;
+exports.amIZombie = amIZombie;
+exports.amIHuman = amIHuman;
+exports.playerExists = playerExists;
+exports.numberOfPlayers = numberOfPlayers;
+exports.playerStatus = playerStatus;
+exports.deletePlayer = deletePlayer;
---
+++ plugins/zi.js
@@ -0,0 +1,274 @@
+var utils = require('utils');
+var ziPlayers = require('ZI/ZIPlayers');
+
+// Game parameters
+var playersNeeded = 2;
+var zombiesPerGame = 1;
+
+//Game state
+var gameInState = {
+	"WAITINGFORPLAYERS" : 0,		//waiting to get enough players
+	"STARTING" : 1,					//Zombies are secretly allocated, countdown
+	"STARTED" : 2,					//Game has stated
+	"CELEBRATING" : 3				//Winner announced, countdown
+};
+var gameState = gameInState["WAITINGFORPLAYERS"];
+
+
+//Countdown function
+function countDown(count) {
+	print("countdown: count = " + count);
+	if (count == 0) {
+		//Time up
+		if (ziPlayers.numberOfPlayers() >= 	playersNeeded) {
+			gameState = gameInState["STARTED"];
+		} else {
+			gameState = gameInState["WAITINGFORPLAYERS"];
+		}
+		updateGameState();
+		return;
+	}
+	
+	//Every second, call again
+	setTimeout( function() {
+		countDown(count-1);
+	}, 1000);
+		
+}
+
+//FSM State transitions
+function updateGameState() {
+
+	switch (gameState) {
+	
+		case gameInState["WAITINGFORPLAYERS"]:		
+			if (ziPlayers.numberOfPlayers() >= 	playersNeeded) {
+				gameState = gameInState["STARTING"];
+			}
+			break;
+		case gameInState["STARTING"]:
+			
+			break;
+
+		case gameInState["STARTED"]:
+			
+			break;
+
+		case gameInState["CELEBRATING"]:
+			break;		
+		default:
+			gameState = gameInState["WAITINGFORPLAYERS"];
+			break;
+	}
+	
+	//Now the Moore outputs
+	gameStateOutputs();
+	
+};
+
+//Moore outputs for the FSM
+function gameStateOutputs() {
+	switch (gameState) {
+	
+		case gameInState["WAITINGFORPLAYERS"]:
+			print("Waiting for players to join");
+			break;
+
+		case gameInState["STARTING"]:
+			print("Game starting soon");
+			countDown(30);
+			break;
+
+		case gameInState["STARTED"]:
+			print("Game on!");
+			break;
+
+		case gameInState["CELEBRATING"]:
+			print("And the winner is?");
+			break;
+					
+		default:
+			print("Error - illegal state");
+			break;
+
+	}
+};
+
+
+//***************Called when the plugin is loaded or refreshed***************
+function startup( plugin ) {
+	print("********** event: startup **********");
+
+	print("initialising infection");
+	//Reset game state
+ 	gameState = gameInState["WAITINGFORPLAYERS"];
+
+	//Add any players already online as human
+	ziPlayers.addAllOnlinePlayersAsHuman();
+	gameStateOutputs();
+	updateGameState();     
+};
+
+events.pluginEnable(startup);
+//****************************************************************************
+
+
+
+
+//***************Event handler for player joining***************
+//Very much depends on game-state 
+function playerJoined( event ) {
+	print("********** event: playerJoined **********");
+	print("GameState: " + gameState);
+	
+	var pl = event.player;
+	echo(pl, "Welcome to infection");
+
+	//Game state dictates type of player
+	if (gameState == gameInState["STARTED"]) {
+		print("A player " + pl.name + " has joined mid game");
+		print("Current list: ", utils.players());
+		
+		//Is this a new player?
+		if (!ziPlayers.playerExists(pl)) {
+			print("Making joining player " + pl.name + " a zombie");
+			ziPlayers.makeZombie(pl);
+		} else {
+			echo(pl, "Welcome back");
+		}
+				
+	} else {
+		ziPlayers.makeHuman(pl);
+	}
+		
+	//Update state 
+	updateGameState();
+
+};
+
+events.playerJoin(playerJoined);
+//**************************************************************
+
+
+
+
+
+//***************Event handler for player leaving***************
+//Depends on state
+function playerLeft( event, cancel ) {
+	print("********** event: playerLeft **********");
+	
+	var pl = event.player;
+	print("Player " + pl.name + " left");
+	ziPlayers.deletePlayer(pl);	
+	updateGameState();
+
+};
+events.on(Packages.org.bukkit.event.player.PlayerQuitEvent, playerLeft);
+
+//*************************************************************************************
+
+//When one entity kills another
+
+function killEvent(event)
+{
+	print("********** event: killEvent **********");
+
+	var entity = event.getEntity();
+	var killer = entity.getKiller();
+	var cause = entity.getLastDamageCause();
+	
+	print(entity + " was killed");	
+	print("Killer is " + killer);
+
+	var entityName = entity.name;
+	var killerName = killer.name;
+	
+	//Has a player been killed?
+	if (!zi.playerExists(entity)) {
+		print("Non player killed");
+		return;
+	}
+	
+	if (gameState != gameInState["STARTED"]) {
+		print("Killed out of game");
+	}
+	
+	//GAME HAS STARTED
+	
+	//Scenario 1: Zombie Kills Human
+	if (ziPlayers.amIZombie(killer) && ziPlayers.amIHuman(entity)) {
+		print("Zombie " + killerName + " has zombified " + entityName);
+		ziPlayer.makeZombie(entity);
+	}
+	//Scenario 2: Human Kills Human
+	else if (ziPlayers.amIHuman(killer) && ziPlayers.amIHuman(entity)) {
+		print("Human " + killerName + " has killed human" + entityName);
+		ziPlayer.makeZombie(killer);
+		echo(killer, "Killing another human is an unforgivable curse! You shall be forever zombie");
+	}		
+	//Scenario 3: Zombie killed a Zombie
+	else if (ziPlayers.amIZombie(killer) && ziPlayers.amIZombie(entity)) {
+		print("Zombie " + killerName + " has killed zombie" + entityName);
+		echo(killer, "Killing another zombie is illogical");
+	}	
+	//Scenario 4: Human killed a Zombie
+	else if (ziPlayers.amIHuman(killer) && ziPlayers.amIZombie(entity)) {
+		print("Human " + killerName + " has killed zombie" + entityName);
+		echo(killer, "Killing a zombie makes you a great warrior.. but it will be back");
+	}	
+	//Catchup
+	else {
+		print("Other cause of death");
+		echo(entity, "Death during the game can only mean eternal zombyness");
+		ziPlayer.makeZombie(killer);
+	}
+	
+	updateGameState();
+			
+};
+
+events.on(Packages.org.bukkit.event.entity.PlayerDeathEvent, killEvent);
+//**************************************************************************************
+
+
+
+
+//Function for monitoring at the console
+function watchdog() {
+	print("ping");
+	ziPlayers.playerStatus();
+	setTimeout(watchdog, 30000);		
+}
+//watchdog();
+
+
+
+// ************************** ADMIN COMMANDS **************************
+function ziStart() {
+	gameState = gameInState["STARTED"];
+	updateGameState();
+}
+function ziStop() {
+	gameState = gameInState["WAITINGFORPLAYERS"];
+	updateGameState();
+}
+function makeZombie( playerName ) {
+	var player = utils.player(playerName);
+	if (player) {
+		ziPlayers.makeZombie(player);
+	}
+}
+function makeHuman( playerName ) {
+	var player = utils.player(playerName);
+	if (player) {
+		ziPlayers.makeHuman(player);
+	}
+}
+
+exports.ziStart = ziStart;
+exports.ziStop = ziStop;
+exports.makeZombie = makeZombie;
+exports.makeHuman = makeHuman;
+
+// TODO - does playerLeft get called when killed? If so, that's a bug
